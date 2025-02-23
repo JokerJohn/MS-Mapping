4b1924e829cf8ebe720206a722743344a1aa1b1e
JokerJohn <2022087641@qq.com>
2025-02-23 17:07:26 +0800

    remove build file

--- ms_mapping/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/xchu/my_git/MS-Mapping/ms_mapping
-# Build directory: /home/xchu/my_git/MS-Mapping/ms_mapping/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
